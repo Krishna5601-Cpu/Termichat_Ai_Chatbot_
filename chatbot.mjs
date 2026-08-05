8dc0e499f843c03e28a54362396a54aeb3189ea1
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-08-05 14:00:51 +0530

    Initialize Ai client

--- chatbot.mjs
+++ chatbot.mjs
@@ -1,1 +1,11 @@
-console.log("Hello Jii");
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import * as readline from "readline";
+
+dotenv.config();
+
+// Initialize Sharanga client
+const client = new OpenAI({
+  apiKey: process.env.SHARANGA_API_KEY,
+  baseURL: "https://sharanga.cloud/api/public/v1",
+});
